3baa605bc498ae9f47362217bda8f751370e7c96
MohammadYM <MohammadYM@LP-MOHAMMADYM>
2020-03-16 22:24:58 +0400

    add login view model  Do some fixes and review

--- KSA.LinkedAcademia/Controllers/LoginController.cs
+++ KSA.LinkedAcademia/Controllers/LoginController.cs
@@ -4,55 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using KSA.LinkedAcademia.Models;
-
 
 namespace KSA.LinkedAcademia.Controllers
 {
-    public class LectureController : Controller
+    public class LoginController : Controller
     {
-        public static List<Lecture> L = new List<Lecture>();
-        public static List<Student> s = new List<Student>();
-        public static Student CR = new Student();
-        static LectureController()
-        {
-            L.Add(new Lecture { Id = 1});
-            L.Add(new Lecture { Id = 2 });
-            L.Add(new Lecture { Id = 3 });
-
-        }
-            
-
-        // GET: Class
+        // GET: Login
         public ActionResult Index()
         {
-            return View(L);
-        }
-
-        // GET: Class/Details/5
-        public ActionResult Details(int id)
-        {
-            Lecture le = L.Select(x => x).Where(y => y.Id == id).FirstOrDefault();
-            
-            return View(le);
-        }
-
-        // GET: Class/Create
-        public ActionResult Create()
-        {
-
             return View();
         }
 
-        // POST: Class/Create
+        // GET: Login/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Lecture lecture)
+        public ActionResult Create(IFormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
-                L.Add(lecture);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -61,22 +44,21 @@ namespace KSA.LinkedAcademia.Controllers
             }
         }
 
-        // GET: Class/Edit/5
+        // GET: Login/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Class/Edit/5
+        // POST: Login/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Lecture lecture)
+        public ActionResult Edit(int id, IFormCollection collection)
         {
             try
             {
                 // TODO: Add update logic here
-                Lecture l = L.Select(x => x).Where(y => y.Id == id).FirstOrDefault();
-                
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -85,15 +67,13 @@ namespace KSA.LinkedAcademia.Controllers
             }
         }
 
-        // GET: Class/Delete/5
+        // GET: Login/Delete/5
         public ActionResult Delete(int id)
         {
-            Lecture le = L.Select(x => x).Where(y => y.Id == id).FirstOrDefault();
-            return View(le);
-            
+            return View();
         }
 
-        // POST: Class/Delete/5
+        // POST: Login/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
@@ -101,7 +81,7 @@ namespace KSA.LinkedAcademia.Controllers
             try
             {
                 // TODO: Add delete logic here
-                L.Remove(L.Select(x => x).Where(y => y.Id == id).FirstOrDefault());
+
                 return RedirectToAction(nameof(Index));
             }
             catch
---
+++ KSA.LinkedAcademia/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSA.LinkedAcademia.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { set; get; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { set; get; }
+
+            
+    }
+}
